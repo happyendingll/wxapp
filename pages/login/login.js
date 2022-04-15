7a47d4752a5478f9c4dfaadaf58c2ded8882d34e
T480Mac <942385056@qq.com>
2022-04-15 17:32:32 +0800

    新增入口和登录页面

---
+++ pages/login/login.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+      tabCur: 0,
+      account: {
+          phone: '',
+          password: ''
+      },
+      login: {
+          phone: '',
+          code: ''
+      },
+      isLoading: false,
+      wxisLoading: false,
+      check: false
+  },
+  tabNavChange(e) {
+      this.setData({
+          tabCur: e.detail.index
+      })
+  },
+  checkChange(e) {
+      this.setData({
+          check: e.detail
+      })
+  },
+})
